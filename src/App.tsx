1b8d5bd212d5b2144348dfe04b6a4a4a0a8e5ffd
jayy1511 <jay1511dj@gmail.com>
2025-09-22 11:55:43 +0200

    feat(frontend): add card type safety and read only data manipulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import CardComp from "./components/CardComp"
+import cards from "./data/cards.json"
+import { useState } from "react"
+
+const [gameCards, setGameCards] = useState(cards)
 
 const App = () => {
-	return (
-		<div>
-			<h1>Memory Game</h1>
-			<CardComp />
-		</div>
-	)
+  // logging the data to the console
+  console.log(cards)
+
+  return (
+    <div className="main_section">
+      <h1>Memory Game</h1>
+      <div className="card_container">
+        {cards.map((card) => {
+          return <CardComp key={card.id} />
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default App
+export default App
